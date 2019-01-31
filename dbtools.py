c4192080d43668f5118880df9b88e12207ce56f0
devnoname120 <devnoname120+gh@gmail.com>
2019-01-31 11:35:25 +0100

    dbtools: add mergenids

--- dbtools.py
+++ dbtools.py
@@ -3,6 +3,7 @@
 import ruamel.yaml as yaml
 import subprocess
 import string
+import regex as re
 
 from glob import glob
 from sys import argv
@@ -43,6 +44,7 @@ class NIDDatabase:
             self.nids["modules"] = {}
     def load(self, nids_db):
         fnids_db = open(nids_db)
+        yaml.allow_duplicate_keys = True
         self.nids = yaml.round_trip_load(fnids_db)
         fnids_db.close()
     
@@ -64,28 +66,60 @@ class NIDDatabase:
     def mergeList(self, nids_lookup, nids_txt):
         fnids_txt = open(nids_txt)
         for entry in fnids_txt:
-            nid, function_name = tuple(filter(None, entry.split(' ')))
+            tpl = tuple(filter(None, re.split(r"\s", entry)))
+            if len(tpl) != 2:
+                print("[warning] Ignoring entry", entry, "because it's not a valid 'nid name'")
+                continue
+
+            nid, function_name = tpl
             nid = int(nid, 0)
 
-            moduleName, module, libraryName, library = nids_lookup.findFunctionByNid(nid)
-            libraryName = re.sub('User$', '', libraryName) 
+            try:
+                moduleName, module, libraryName, library, functionName = nids_lookup.findFunctionByNid(nid)
+            except IndexError:
+                print("[error] Function", function_name, "with NID", '0x{:08X}'.format(nid), "not found in lookup, ignoring...")
+                continue
 
-            db_module = None
-            for cModuleName, cModule in self.findModule(module):
-                db_module = cModule
-            
-            if not db_module:
-                db_module = self.addModule(moduleName, module["nid"])
+            try:
+                dbModuleName, dbModule, dbLibraryName, dbLibrary, dbFunctionName = self.findFunctionByNid(nid)
+
+                # Ignore k prefix and ForUser suffix differences
+                if dbFunctionName != function_name and dbFunctionName != "k" + function_name and dbFunctionName + "ForUser" != function_name:
+                    print("[warning] Function", function_name, "found with different name in db:", dbFunctionName, ", skipping...")
+                continue
+            except IndexError:
+                pass
+
+
             db_library = None
-            for cLibraryName, cLibrary in self.findLibrary(libraryName, module)
+            for cLibraryName, cLibrary in self.findLibraryByNid(library['nid']):
+                assert db_library is None # There should be only one corresponding library
                 db_library = cLibrary
 
-            if not db_library:
-                db_library = self.addLibrary(db_module, libraryName, library["nid"])
-                self.setLibraryKernel(db_library, module["kernel"])
-            
-            db_function = None
-            for 
+            if db_library is None:
+                db_module = None
+                for cModuleName, cModule in self.findModule(module):
+                    assert db_module is None  # There should be only one corresponding module
+                    db_module = cModule
+
+                if db_module is not None:
+                    newLib = self.addLibrary(db_module, libraryName, library['nid'])
+                    self.setLibraryKernel(newLib, library["kernel"])
+
+                    self.addFunctionWithPrefixSuffix(newLib, function_name, nid)
+                else:
+                    newModule = self.addModule(moduleName, module['nid'])
+                    newLib = self.addLibrary(newModule, libraryName, library['nid'])
+                    self.setLibraryKernel(newLib, library["kernel"])
+
+                    self.addFunctionWithPrefixSuffix(newLib, function_name, nid)
+            else:
+                self.addFunctionWithPrefixSuffix(db_library, function_name, nid)
+
+
+            # Commented this because I don't know what it's used for
+            # libraryName = re.sub('User$', '', libraryName)
+
             
 
     def setModuleNid(self, module, nid):
@@ -105,20 +139,16 @@ class NIDDatabase:
         return False
 
     def addModule(self, name, nid):
+        if not self.nids["modules"]:
+            self.nids["modules"] = {}
+
         try:
-            setModuleNid(name, nid)
-            return mod
+            self.nids["modules"][name]
         except:
-            if not self.nids["modules"]:
-                self.nids["modules"] = {}
+            self.nids["modules"][name] = {}
 
-            try:
-                self.nids["modules"][name]
-            except:
-                self.nids["modules"][name] = {}
-
-            self.setModuleNid(self.nids["modules"][name], nid)
-            return self.nids["modules"][name]
+            self.nids["modules"][name]["nid"] = nid
+        return self.nids["modules"][name]
 
     def removeModule(self, module_name):
         for modu in self.findModule(module_name):
@@ -130,28 +160,35 @@ class NIDDatabase:
 
     def addLibrary(self, module, name, nid):
         # db.yml never has a User suffix
-        name = re.sub('User$', '', name) 
-        try:
-            self.findLibrary(name, module) # Does not raise exception if library already exists
-            setLibraryNid(name, nid, module)
-            return lib
-        except:
-            try:
-                module["libraries"]
-            except:
-                module["libraries"] = {}
-            
-            try:
-                module["libraries"][name]
-            except:
-                module["libraries"][name] = {}
+        # name = re.sub('User$', '', name)
 
-            module["libraries"][name]["nid"] = HexWInt(nid, 8)
-            return module["libraries"][name]
+        try:
+            module["libraries"]
+        except:
+            module["libraries"] = {}
+
+        try:
+            module["libraries"][name]
+        except:
+            module["libraries"][name] = {}
+
+        module["libraries"][name]["nid"] = HexWInt(nid, 8)
+        return module["libraries"][name]
+
+    def addFunctionWithPrefixSuffix(self, library, name, nid):
+        if library["kernel"] == True:
+            if not name.startswith("sce"):
+                print("[warning] Setting k prefix to function with unknown prefix:", name)
+            name = "k" + name
+        if name.endswith("ForKernel") or name.endswith("ForDriver"):
+            if library["kernel"] != True:
+                print("[error] Function", name, "has a kernel prefix but is not in a library marked kernel")
+            name = name[:-len("ForKernel")] # Remove suffix
+
+        self.addFunction(library, name, nid)
 
     def addFunction(self, library, name, nid):
         try:
-            library[name] #for lib in self.findFunction(name, module): # Function already exists
             library[name] = HexWInt(nid)
             return library[name]
         except:
@@ -165,12 +202,18 @@ class NIDDatabase:
 
     def modules(self):
         modules = self.nids["modules"]
+        if modules is None:
+            return
+
         for cModule in modules:
             yield (cModule, modules[cModule])
 
     def libraries(self, module=None):
         
-        if module != None:
+        if module is not None:
+            if not "libraries" in module or module["libraries"] is None:
+                return # this module has no libraries, it's ok
+
             libraries = module["libraries"]
             for cLibrary in libraries:
                 yield (cLibrary, libraries[cLibrary])
@@ -195,13 +238,25 @@ class NIDDatabase:
             # User libraries are suffixed with 'User' but this suffix is not in db.yml
             if cLibraryName == library or cLibraryName == library + "User" :
                 yield (cLibraryName, cLibrary)
+
+    def findLibraryByNid(self, nid):
+        for cModuleName, cModule in self.modules():
+            for cLibraryName, cLibrary in self.libraries(cModule):
+                # User libraries are suffixed with 'User' but this suffix is not in db.yml
+                if cLibrary['nid'] == nid :
+                    yield (cLibraryName, cLibrary)
+
     def findFunctionByNid(self, nid):
         for cModuleName, cModule in self.modules():
             for cLibraryName, cLibrary in self.libraries(cModule):
-                funcs = cLibrary["functions"]:
+                if not "functions" in cLibrary or cLibrary["functions"] is None:
+                    continue # This library has no functions
+
+                funcs = cLibrary["functions"]
                 for func in funcs:
-                    if int(funcs[func], 0) == nid:
-                        return cModuleName, cModule, cLibraryName, cLibrary
+                    if funcs[func] == nid:
+                        return cModuleName, cModule, cLibraryName, cLibrary, func
+        raise IndexError("Not found")
                     
 if __name__ == "__main__":
     if argv[1] == "fixnids":
@@ -210,4 +265,11 @@ if __name__ == "__main__":
 
         db.fixNids(db_lookup)
         db.save("dbfixed.yml")
+
+    elif argv[1] == "mergenids":
+        db = NIDDatabase("db.yml")
+        db_lookup = NIDDatabase("db_lookup.yml")
+
+        db.mergeList(db_lookup, "nids.txt")
+        db.save("dbmerged.yml")
 
